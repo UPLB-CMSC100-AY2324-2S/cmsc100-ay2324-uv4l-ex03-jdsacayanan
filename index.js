b5e12c543747c1152ed1d2e5970262c8c77184f3
jdscayanan <jdsacayanan@up.edu.ph>
2024-03-07 08:03:04 +0800

    refactor: changed function and variable names in storePassword()

--- index.js
+++ index.js
@@ -1,79 +1,88 @@
 function validatePassword(firstInput, secondInput) {
-    // The two passwords match
-    if (firstInput!==secondInput) {
-        return false;
-    }
+  // The two passwords match
+  if (firstInput !== secondInput) {
+    return false;
+  }
 
-    password = String(firstInput);
+  password = String(firstInput);
 
-    // the password has at least 8 character
-    if (password.length<8) {
-        return false;
-    }
+  // the password has at least 8 character
+  if (password.length < 8) {
+    return false;
+  }
 
-    // the password has at least 1 number
-    valid = false;
-    for (let i=0; i<password.length; i++) {
-      if (!isNaN(parseInt(password[i]))) {  // checks if the number is a number (or integer since if a single character can always be parsed as an integer if it is from 0-9)
-        valid = true;
-        break;
-      }
+  // the password has at least 1 number
+  valid = false;
+  for (let i = 0; i < password.length; i++) {
+    if (!isNaN(parseInt(password[i]))) {
+      // checks if the number is a number (or integer since if a single character can always be parsed as an integer if it is from 0-9)
+      valid = true;
+      break;
     }
-    if (!valid) {
-        return false;
-    }
+  }
+  if (!valid) {
+    return false;
+  }
 
-    // the password has 1 uppercase character
-    valid = false;
-    for (let i=0; i<password.length; i++) {
-        if (String(password[i])==String(password[i]).toUpperCase() && isNaN(parseInt(password[i]))) {   // checks if the character is not a number and is uppercase
-            valid = true;
-            break;
-        }
+  // the password has 1 uppercase character
+  valid = false;
+  for (let i = 0; i < password.length; i++) {
+    if (
+      String(password[i]) == String(password[i]).toUpperCase() &&
+      isNaN(parseInt(password[i]))
+    ) {
+      // checks if the character is not a number and is uppercase
+      valid = true;
+      break;
     }
-    if (!valid) {
-        return false;
-    }
+  }
+  if (!valid) {
+    return false;
+  }
 
-    // the password has 1 lowercase character
-    valid = false;
-    for (let i=0; i<password.length; i++) {
-        if (String(password[i])==String(password[i]).toLowerCase() && isNaN(parseInt(password[i]))) {   // checks if the character is not a number and it is lowercase
-            valid = true;
-            break;
-        }
+  // the password has 1 lowercase character
+  valid = false;
+  for (let i = 0; i < password.length; i++) {
+    if (
+      String(password[i]) == String(password[i]).toLowerCase() &&
+      isNaN(parseInt(password[i]))
+    ) {
+      // checks if the character is not a number and it is lowercase
+      valid = true;
+      break;
     }
-    if (!valid) {
-        return false;
-    }
+  }
+  if (!valid) {
+    return false;
+  }
 
-    // all checks passed
-    return true;
+  // all checks passed
+  return true;
 }
 
 function reversePassword(input) {
-    // ensure that the variable is a string
-    input = String(input);
+  // ensure that the variable is a string
+  input = String(input);
 
-    // a variable to store the reversed password
-    reversed = "";
-    
-    // loop through all the characters of the password
-    for (let i=(input.length-1); i>=0; i--) {
-        reversed = reversed+input[i];
-    }
+  // a variable to store the reversed password
+  reversed = "";
 
-    // return reversed password
-    return reversed;
+  // loop through all the characters of the password
+  for (let i = input.length - 1; i >= 0; i--) {
+    reversed = reversed + input[i];
+  }
+
+  // return reversed password
+  return reversed;
 }
 
-function storeToObject(givenName, firstInput, secondInput) {
-    person = {
-        name: givenName,
-        password: firstInput
-    }
-    if(validatePassword(firstInput, secondInput)) {
-        person.password = reversePassword(firstInput);
-    }
-    return person;
+function storePassword(givenName, firstInput, secondInput) {
+  person = {
+    name: givenName,
+    newpassword: firstInput,
+  };
+  if (validatePassword(firstInput, secondInput)) {
+    person.newpassword = reversePassword(firstInput);
+  }
+  return person;
 }
